f8e0f047afd462e225cb919ce125a29ab5452a97
Vivek <99064174+Developer01042003@users.noreply.github.com>
2023-10-10 14:34:21 +0530

    Update settings.py

--- customadmin/customadmin/settings.py
+++ customadmin/customadmin/settings.py
@@ -79,12 +79,8 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'campdata-david',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',  # Change this if your MySQL server is running on a different host
-        'PORT': '3306',       # Change this if your MySQL server is using a different port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 # Password validation
